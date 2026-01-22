8c7b46abffe4d3fbc01293b8578dcb4c56d6c1e8
Luan Lara <122845430+luanferreiradev@users.noreply.github.com>
2026-01-22 16:26:17 -0300

    Adicionando restrições na cláusula where com Criteria API

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -3,15 +3,15 @@ package com.algaworks.algafood.infrastructure.repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -26,7 +26,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
         CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
 
         CriteriaQuery<Restaurante> criteriaQuery = criteriaBuilder.createQuery(Restaurante.class);
-        criteriaQuery.from(Restaurante.class); //from Restaurante
+        Root<Restaurante> root = criteriaQuery.from(Restaurante.class); //from Restaurante
+
+        Predicate nomePredicate = criteriaBuilder.like(root.get("nome"), "%" + nome +"%");
+
+        Predicate taxaInicialPredicate = criteriaBuilder.greaterThan(root.get("taxaFrete"), taxaFreteInicial);
+
+        Predicate taxaFinalPredicate = criteriaBuilder.lessThan(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteriaQuery.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 
         TypedQuery<Restaurante> query = manager.createQuery(criteriaQuery);
         return query.getResultList();
