b2b7177cacb7e5199e24b33715d714d2d84d506e
Luan Lara <122845430+luanferreiradev@users.noreply.github.com>
2026-02-06 13:33:24 -0300

    8.2. Lançando exceções customizadas anotadas com @ResponseStatus

--- src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,6 +1,11 @@
 package com.algaworks.algafood.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
 public class EntidadeNaoEncontradaException extends RuntimeException {
+
     private static final Long serialVersionUID = 1L;
     public EntidadeNaoEncontradaException(String message) {
         super(message);
